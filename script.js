4e6e13fd6ee1b4970ff00dc2cfe7c3f06e1522a3
Filipp Goriainov <barrios1991phil@gmail.com>
2022-06-25 15:57:01 +0400

    node??

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
 
 
 const swiper = new Swiper('.swiper', {
-
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -10,16 +11,5 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
     },
 });
